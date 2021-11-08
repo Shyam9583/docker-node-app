73dc50ddab49ad8037909439c0474d64839ad29e
shyam <strikerallin1@gmail.com>
2021-11-08 17:38:25 +0530

    Created a basic CRUD app

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-const app = require("express")();
+const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
+
+const app = express();
+
+const postRouter = require("./routes/postRoutes");
+
+const userRouter = require("./routes/userRoutes");
 
 const port = process.env.PORT || 3000;
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("Successfully connected to the database"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send({
@@ -11,5 +40,8 @@ app.get("/", (req, res) => {
     pincode: "769003",
   });
 });
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
